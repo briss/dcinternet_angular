23b9d8338556595e6edec87189918727d6dd2d02
brissgs <briseidags@gmail.com>
2025-09-08 19:47:41 -0600

    Mover estilos a css, agregar profile, desplegar arreglo de cursos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hola_mundo');
+  protected readonly titulo = signal('hola_mundo');
+  protected readonly hola = 'Hola';
+
+  protected cursos = [
+    { 'nombre': 'Angular', 'link': 'https://www.dcinternet.com.mx/dciCursosdeAngular.jsp' },
+    { 'nombre': 'React', 'link': 'https://www.dcinternet.com.mx/dciCursosdeAngular.jsp' },
+    { 'nombre': 'JavaScript', 'link': 'https://www.dcinternet.com.mx/dciCursosdeAngular.jsp' },
+    { 'nombre': 'TypeScript', 'link': 'https://www.dcinternet.com.mx/dciCursosdeAngular.jsp' }
+  ];
 }
