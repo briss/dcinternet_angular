2a4c8faff366262e81d6786ebd314fe7fe68bbae
brissgs <briseidags@gmail.com>
2025-09-10 21:09:24 -0600

    Cambiar app por template

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Template } from './app/template/template';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(Template, appConfig)
   .catch((err) => console.error(err));
