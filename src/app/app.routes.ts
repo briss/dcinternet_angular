cd9de45a7d4203f31ff4a864d435e9d6cd79aa1d
brissgs <briseidags@gmail.com>
2025-09-18 11:11:36 -0600

    Agregar componente menú, ajustar rutas, actualizar ignored

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { Profile } from './profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'profile', component: Profile },
+    { path: 'inicio', component: App }
+];
