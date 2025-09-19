06a741a7060e7c07d76f70d98552c41db46fe3d1
brissgs <briseidags@gmail.com>
2025-09-19 15:09:14 -0600

    Integrar el pie de página al menú

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { PieDePagina } from "../pie-de-pagina/pie-de-pagina";
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, PieDePagina],
   templateUrl: './menu.html',
   styleUrl: './menu.css'
 })
