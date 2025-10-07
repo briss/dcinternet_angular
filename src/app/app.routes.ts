43baaa7b2bbca8f76255b56cd9cfef38c6b7b888
brissgs <briseidags@gmail.com>
2025-10-07 09:14:22 -0600

    Agregar rutas para distintos ejemplos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { App } from './app';
 import { Profile } from './profile/profile';
 import { Avatar } from './avatar/avatar';
 import { Card } from './card/card';
+import { ListaCursos } from './lista-cursos/lista-cursos';
+import { AfterContentPadre } from './hooks/after-content-padre/after-content-padre';
+import { HolaMundo } from './observables/hola-mundo/hola-mundo';
 
 export const routes: Routes = [
     { path: 'profile', component: Profile },
+    { path: 'cursos', component: ListaCursos },
     { path: 'inicio', component: App },
     { path: 'card', component: Card },
+    { path: 'afterPadre', component: AfterContentPadre },
+    { path: 'observables', component: HolaMundo },
 
     { path: '**', redirectTo: 'inicio' }
 ];
