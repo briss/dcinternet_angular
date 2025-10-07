d7a3a8bfdf6898e8bebf85c0bfab19411d8a8841
brissgs <briseidags@gmail.com>
2025-10-07 09:09:07 -0600

    Completa ejemplo fetch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,7 +1,11 @@
 function buscarCurso() {
   fetch("https://www.dcinternet.com.mx/rino/cursos/301")
     .then(resultado => {
-      console.log(resultado);
+      //console.log(resultado);
+      return resultado.json();
+    })
+    .then(res => {
+      console.log(res);
     })
     .catch(error => {
       console.log(error);
