532150ec27bc89758055be8599a10a01cb7b7f5d
brissgs <briseidags@gmail.com>
2025-10-08 17:39:35 -0600

    Agregar componente dinámicamente

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, ElementRef, Signal, viewChild, ViewContainerRef } from '@angular/core';
 import { Alumno } from '../modelo/alumno.modelo';
 import { Persona } from '../modelo/persona.modelo';
 import { Card } from "../card/card";
 import { Avatar } from "../avatar/avatar";
 import { FormsModule } from '@angular/forms';
+import { ListaTelefonos } from '../lista-telefonos/lista-telefonos';
 
 @Component({
   selector: 'app-profile',
@@ -11,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {
+export class Profile implements AfterViewInit, AfterContentChecked {
 //  protected nombre = 'Briseida';
 //  protected correo = 'briseidags';
 //  protected imagen:string|undefined;
@@ -23,8 +24,25 @@ export class Profile {
   protected mensajeHijo:string|undefined;
   protected foto_tamanio = 100;
 
+  protected nombre:Signal<ElementRef|undefined> = viewChild("nombreInput");
+
+  constructor(private viewContainerRef:ViewContainerRef) {
+  }
+  
+  ngAfterContentChecked(): void {
+    console.log("Profile.afterContentCheck");
+  }
+
+  ngAfterViewInit() {
+    this.nombre()?.nativeElement.focus();
+  }
 
   recibirMensaje(event:string) {
     this.mensajeHijo = event;
+  }
+
+  mostrarTelefonos() {
+    this.viewContainerRef.clear();
+    this.viewContainerRef.createComponent(ListaTelefonos);
   }
 }
