97c3a630e9cae1e84e3c6cc2af8e0ba1b8712090
brissgs <briseidags@gmail.com>
2025-10-08 17:36:19 -0600

    Agregar ejemplo observables

--- src/app/service/curso.service.ts
+++ src/app/service/curso.service.ts
@@ -1,20 +1,29 @@
-import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from "@angular/core";
 import { Curso } from "../modelo/curso.modelo";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CursoService {
 
+    private cursosUrl = "https://www.dcinternet.com.mx/rino/cursos";
+    private httpClient = inject(HttpClient);
+
     async getCursos():Promise<Curso[]> {
         let cursos;
         try {
-            let resultado = await fetch("https://www.dcinternet.com.mx/rino/cursos");
+            let resultado = await fetch(this.cursosUrl);
             cursos = await resultado.json();
         } catch(error) {
             console.log(error);
         }
 
         return cursos;
+    }
+
+    getCursosObservable():Observable<Curso[]> {
+        return this.httpClient.get<Curso[]>(this.cursosUrl);
     }
 }
