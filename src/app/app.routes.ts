9380527d05fa581526a4978c4f15829240ab1f12
brissgs <briseidags@gmail.com>
2025-10-14 11:51:56 -0600

    Poner los nuevos ejercicios en el menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,19 @@ import { ListaCursos } from './lista-cursos/lista-cursos';
 import { AfterContentPadre } from './hooks/after-content-padre/after-content-padre';
 import { HolaMundo } from './observables/hola-mundo/hola-mundo';
 import { CursoObservable } from './observables/curso-observable/curso-observable';
+import { CursoResources } from './resources/curso-resources/curso-resources';
+import { Buscador } from './buscador/buscador';
 
 export const routes: Routes = [
     { path: 'profile', component: Profile },
     { path: 'cursos', component: ListaCursos },
     { path: 'cursosObservable', component: CursoObservable },
+    { path: 'cursosResources', component: CursoResources },
     { path: 'inicio', component: App },
     { path: 'card', component: Card },
     { path: 'afterPadre', component: AfterContentPadre },
     { path: 'observables', component: HolaMundo },
+    { path: 'buscador', component: Buscador },
 
     { path: '**', redirectTo: 'inicio' }
 ];
