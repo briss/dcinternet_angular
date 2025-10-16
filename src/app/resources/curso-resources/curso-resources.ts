b78b6c9edb10ff92ce075a8be82612d6fcd3b041
Briseida Galvan Suarez <bgalvan@banxico.org.mx>
2025-10-16 14:16:43 -0600

    Cambiar la lista a una tabla y agregar botones de edición y borrado. Agregar link para desplegar detalles del curso

--- src/app/resources/curso-resources/curso-resources.ts
+++ src/app/resources/curso-resources/curso-resources.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject, input, resource } from '@angular/core';
 import { Curso } from '../../modelo/curso.modelo';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { CursoService } from '../../service/curso.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-curso-resources',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './curso-resources.html',
   styleUrl: './curso-resources.css'
 })
@@ -28,10 +29,21 @@ export class CursoResources {
     stream: () => this.cursoService.getCursosObservable(),
     defaultValue: []
   });
-  
+
 //  cursosResource = resource({
 //    loader: () => fetch("https://www.dcinternet.com.mx/rino/cursos")
 //  });
 
 
+  borrarCurso(idCurso:number) {
+    let respuesta = confirm("Estas seguro que quieres borrar el curso con id " + idCurso);
+    if (respuesta) {
+      this.cursoService.borrarCurso(idCurso);
+      this.cursosRxResource.reload();
+    }
+  }
+
+  editarCurso(curso:Curso) {
+    console.log(curso);
+  }
 }
