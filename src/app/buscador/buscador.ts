65f8845089bec67011743a6ba99e030229df8b57
Briseida Galvan Suarez <bgalvan@banxico.org.mx>
2025-10-16 14:17:46 -0600

    Agregar en la pantalla del buscador la forma para registrar cursos.

--- src/app/buscador/buscador.ts
+++ src/app/buscador/buscador.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { CursoResources } from "../resources/curso-resources/curso-resources";
+import { RegistroCursoTemplate } from "../registro-curso-template/registro-curso-template";
 
 @Component({
   selector: 'app-buscador',
-  imports: [FormsModule, CursoResources],
+  imports: [FormsModule, CursoResources, RegistroCursoTemplate],
   templateUrl: './buscador.html',
   styleUrl: './buscador.css'
 })
