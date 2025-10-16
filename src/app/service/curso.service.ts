08ced93528f08dd4d0cdf073686b3d83ec73d6d8
Briseida Galvan Suarez <bgalvan@banxico.org.mx>
2025-10-16 14:11:03 -0600

    Agregar método para borrar y obtener un único curso

--- src/app/service/curso.service.ts
+++ src/app/service/curso.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from "@angular/core";
+import { inject, Injectable, signal, Signal } from "@angular/core";
 import { Curso } from "../modelo/curso.modelo";
 import { Observable } from "rxjs";
 
@@ -10,6 +10,15 @@ export class CursoService {
 
     private cursosUrl = "https://www.dcinternet.com.mx/rino/cursos";
     private httpClient = inject(HttpClient);
+
+    private cursoActual = signal<Curso>({
+        id: 0,
+        nombre: '',
+        categoria: '',
+        duracion: 0,
+        descripcion: '',
+        imagen: ''
+    });
 
     async getCursos():Promise<Curso[]> {
         let cursos;
@@ -30,5 +39,15 @@ export class CursoService {
     registrar(curso:Curso) {
         this.httpClient.post(this.cursosUrl, curso)
             .subscribe(() => console.log("Curso registrado"));
+    }
+
+    getCurso(id:string):Observable<Curso> {
+        return this.httpClient.get<Curso>(this.cursosUrl + '/' + id);
+    }
+
+    borrarCurso(id:number):void {
+        this.httpClient.delete(this.cursosUrl + '/' + id).subscribe({
+            next: (response) => console.log(response)
+        });
     }
 }
